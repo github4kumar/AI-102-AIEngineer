6e821fefea1aa2e8ba64d4642c7cb5a5a29b6c21
github4kumar <kumar.condo@gmail.com>
2023-06-29 08:59:32 -0700

    extract data from form- train test model

--- 21-custom-form/setup.cmd
+++ 21-custom-form/setup.cmd
@@ -2,9 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variable values 
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set resource_group=YOUR_RESOURCE_GROUP
-set location=YOUR_LOCATION_NAME
+set subscription_id="102T00 CSR 3"
+set resource_group=ResourceGroup1lod32149443
+set location=eastus
 set expiry_date=2024-01-01T00:00:00Z
 
 rem Get random numbers to create unique resource names
